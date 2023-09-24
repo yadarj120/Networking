d45eb8136bead02e079f369dcbc3b11a4e296a19
Arjun Yadav <yadarj120@gmail.com>
2023-09-24 12:59:47 +0530

    removed problem with formatting in server

--- server.py
+++ server.py
@@ -4,11 +4,12 @@ import threading
 PORT = 5050
 HEADER = 64
 FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 server_addr = socket.gethostbyname(socket.gethostname())
 
 ADDR = (server_addr, PORT)
 
-server_sock = socket.socket();
+server_sock = socket.socket(socket.AF_INET, socket.SOCKET_STREAM);
 server_sock.bind(ADDR);
 
 def handle_client(client_sock, client_addr):
@@ -16,8 +17,13 @@ def handle_client(client_sock, client_addr):
     connected = True
     while connected:
         enc_msg_length = int(client_sock.recv(HEADER).decode(FORMAT))
-        msg = client_sock.recv(enc_msg_length).decode()
-        print(msg)
+        msg = client_sock.recv(enc_msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f"[{client_addr}] {msg}")
+    
+    client_sock.close()
 
 
 def start():
@@ -26,7 +32,11 @@ def start():
         client_sock, client_addr = server_sock.accept()
         client_thread = threading.Thread(target=handle_client, args=(client_sock, client_addr))
         client_thread.start();
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
 
+print("[STARTING] server is starting ...")  
 start()
+
+
 
 
